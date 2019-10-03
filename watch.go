d5fbd38ecb290234779432adf5236706454a400c
fujiokayu <fujiokayu@gmail.com>
2019-10-03 21:01:39 +0900

    add mock functions.

--- watch.go
+++ watch.go
@@ -5,8 +5,28 @@ import (
 	"time"
 )
 
-func watch() {
+// Duration is exported gloval value
+type Duration int64
+
+var sp time.Time
+var ep time.Time
+
 // Watch : mock function
 func Watch() {
 	fmt.Println(time.Now())
+}
+
+// Start : mock function
+func Start() {
+	sp = time.Now()
+}
+
+// Stop : mock function
+func Stop() {
+	ep = time.Now()
+}
+
+// GetDuration : show time since start
+func GetDuration() {
+	fmt.Println(time.Since(sp))
 }
